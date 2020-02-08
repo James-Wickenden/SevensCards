a59bb438b088973a3c9dff308eff49ae8dadc858
James Wickenden <jameswickenden14@gmail.com>
2020-02-08 16:22:10 +0000

    Formatting to allow COM difficulties and bugfixing

--- SevensCards/Player_COM.vb
+++ SevensCards/Player_COM.vb
@@ -9,15 +9,43 @@
         Me.difficulty = difficulty
     End Sub
 
-    Private Function GetPlayedCard_COM() As Card
+    Private Function GetValidCards() As Card()
+        Dim res As New List(Of Card)
         For Each card As Card In hand.GetHand
             If card.GetValid Then
-                Threading.Thread.Sleep(300)
-                Return card
+                res.Add(card)
             End If
         Next
+        Return res.ToArray
+    End Function
+
+    Private Function PickCard_0() As Card
+        Dim validCards() As Card = GetValidCards()
+        If validCards.Length = 0 Then Return Nothing
+
+        Randomize()
+        Return validCards(Int((validCards.Count) * Rnd()))
+    End Function
+
+    Private Function PickCard_1() As Card
+
+    End Function
+
+    Private Function PickCard_2() As Card
+
+    End Function
+
+    Private Function GetPlayedCard_COM() As Card
+        Dim card As Card
+
+        Select Case difficulty
+            Case 0 : card = PickCard_0()
+            Case 1 : card = PickCard_1()
+            Case 2 : card = PickCard_2()
+            Case Else : card = PickCard_0()
+        End Select
         Threading.Thread.Sleep(300)
-        Return Nothing
+        Return card
     End Function
 
     Public Overrides Sub GetMove()
@@ -28,6 +56,11 @@
             End If
         End While
         Dim card As Card = GetPlayedCard_COM()
-        callback(card)
+
+        Try
+            callback(card)
+        Catch ex As Exception
+        End Try
+
     End Sub
 End Class
--- SevensCards/TODO.vb
+++ SevensCards/TODO.vb
@@ -6,7 +6,5 @@
     '   Implement console/log for tracking/ online debugging
 
     'BUGS:
-    '   Can crash when exiting on a COM move
-    '       This is likely due to the main thread closing and the COM move thread trying to invoke it.
     '   
 End Class
