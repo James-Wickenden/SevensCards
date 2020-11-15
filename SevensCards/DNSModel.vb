8ef4e99799f4b568d1e55ac71a016fd7226e9eb9
James Wickenden <jameswickenden14@gmail.com>
2020-11-15 22:17:39 +0000

    Added Server IP listing

--- SevensCards/DNSModel.vb
+++ SevensCards/DNSModel.vb
@@ -1,4 +1,6 @@
-﻿Public Class DNSModel
+﻿Imports System.Net
+
+Public Class DNSModel
     Private dnsView As DNSView
 
     Public Sub New(menu As Menu)
@@ -9,5 +11,36 @@
 
     End Sub
 
+    Public Sub SetViewInModel(dnsView As DNSView)
+        Me.dnsView = dnsView
+    End Sub
 
+    Public Sub StartServer()
+
+    End Sub
+
+    Public Sub ListDNS_Addresses(Optional Verbose As Boolean = True)
+        Dim hostName As String = System.Net.Dns.GetHostName()
+        Dim addresses As IPAddress() = Dns.GetHostEntry(hostName).AddressList()
+
+        If Verbose Then dnsView.WriteToLog(dnsView.serverInfo, "All Machine IP Addresses: ")
+        For Each hostAdr As IPAddress In addresses
+            If hostAdr.ToString().Contains("192.168") Then
+                ListDNS_Address(hostName, hostAdr.ToString())
+                dnsView.WriteToLog(dnsView.serverInfo, "    [Local Network IP]")
+            Else
+                If Verbose Then
+                    ListDNS_Address(hostName, hostAdr.ToString())
+                    If hostAdr.AddressFamily.ToString() = "InterNetwork" Then
+                        dnsView.WriteToLog(dnsView.serverInfo, "    [Potential host IP]")
+                    End If
+                End If
+
+            End If
+        Next
+    End Sub
+
+    Private Sub ListDNS_Address(hostName As String, ipAddress As String)
+        dnsView.WriteToLog(dnsView.serverInfo, "Name: " & hostName & " IP Address: " & ipAddress)
+    End Sub
 End Class
--- SevensCards/DNSView.vb
+++ SevensCards/DNSView.vb
@@ -1,6 +1,5 @@
 ﻿Public Class DNSView
     Private fp As New FunctionPool
-    Private serverPanel, clientPanel As New Panel
     Private dnsModel As DNSModel
     Public clientInfo, serverInfo As TextBox
 
@@ -11,6 +10,7 @@
 
     Public Sub SetDNSModel(dnsModel As DNSModel)
         Me.dnsModel = dnsModel
+        dnsModel.SetViewInModel(Me)
     End Sub
 
     Public Sub WriteToLog(log As TextBox, str As String)
@@ -21,17 +21,17 @@
     End Sub
 
     Private Sub PanelSetup()
+        Dim serverPanel, clientPanel As New Panel
         Dim serverBut As New Button
         fp.objectHandler.AddButton(Me, serverBut, 20, Me.Width / 2 + 20, 50, 120, "Host Server",
-                                   New Action(Sub() ServerPanelSetup(serverBut)))
-
-        ClientPanelSetup()
+                                   New Action(Sub() ServerPanelSetup(serverBut, serverPanel)))
+        ClientPanelSetup(clientPanel)
     End Sub
 
-    Private Sub ServerPanelSetup(sender As Button)
+    Private Sub ServerPanelSetup(sender As Button, serverPanel As Panel)
         sender.Dispose()
 
-        fp.objectHandler.AddObject(Me, serverPanel, 0, Me.Width / 2, Me.Height, Me.Width / 2, "Server")
+        fp.objectHandler.AddObject(Me, serverPanel, 0, Me.Width / 2, Me.Height, Me.Width / 2, "")
         serverPanel.BorderStyle = BorderStyle.Fixed3D
         serverPanel.BackColor = Me.BackColor
 
@@ -40,13 +40,17 @@
         serverLabel.Font = New Font("Arial", 25)
 
         serverInfo = New TextBox
-        fp.objectHandler.AddObject(serverPanel, serverInfo, 80, 20, clientPanel.Height / 2,
-                                   serverPanel.Width - 40, "Server IP Info")
-        LogSetup(serverInfo)
+        fp.objectHandler.AddObject(serverPanel, serverInfo, 80, 20, serverPanel.Height / 2,
+                                   serverPanel.Width - 60, " ")
+        LogSetup(serverInfo, "Server IP Info")
+
+        StartServerPanelSetup(serverPanel)
+
+        dnsModel.ListDNS_Addresses(False)
     End Sub
 
-    Private Sub ClientPanelSetup()
-        fp.objectHandler.AddObject(Me, clientPanel, 0, 0, Me.Height, Me.Width / 2, "Client")
+    Private Sub ClientPanelSetup(clientPanel As Panel)
+        fp.objectHandler.AddObject(Me, clientPanel, 0, 0, Me.Height, Me.Width / 2, "")
         clientPanel.BorderStyle = BorderStyle.Fixed3D
         clientPanel.BackColor = Me.BackColor
 
@@ -56,23 +60,24 @@
 
         clientInfo = New TextBox
         fp.objectHandler.AddObject(clientPanel, clientInfo, 80, 20, clientPanel.Height / 2,
-                                   clientPanel.Width - 60, "Client IP Info ")
-        LogSetup(clientInfo)
+                                   clientPanel.Width - 40, " ")
+        LogSetup(clientInfo, "Client IP Info")
 
-        ConnectPanelSetup()
+        ConnectPanelSetup(clientPanel)
 
     End Sub
 
-    Private Sub ConnectPanelSetup()
+    Private Sub ConnectPanelSetup(clientPanel As Panel)
         Dim connectPanel As New Panel
         fp.objectHandler.AddObject(clientPanel, connectPanel, clientPanel.Height / 2 + 100, 20,
-                                   clientPanel.Height / 3 - 20, clientPanel.Width - 60, "")
+                                   clientPanel.Height / 3 - 20, clientPanel.Width - 40, "")
         connectPanel.BorderStyle = BorderStyle.FixedSingle
         connectPanel.BackColor = Color.Green
 
         Dim connectIP_lbl As New Label
         fp.objectHandler.AddObject(connectPanel, connectIP_lbl, 20, 20, 30, 120, "IP Address:")
         connectIP_lbl.Font = New Font("Arial", 15)
+        connectIP_lbl.TextAlign = ContentAlignment.MiddleRight
 
         Dim connectIP_txt As New TextBox
         fp.objectHandler.AddObject(connectPanel, connectIP_txt, 20, 160, 30, 120, "")
@@ -83,13 +88,37 @@
 
     End Sub
 
-    Private Sub LogSetup(Log As TextBox)
+    Private Sub StartServerPanelSetup(serverPanel As Panel)
+        Dim hostPanel As New Panel
+        fp.objectHandler.AddObject(serverPanel, hostPanel, serverPanel.Height / 2 + 100, 20,
+                                   serverPanel.Height / 3 - 20, serverPanel.Width - 60, "")
+        hostPanel.BorderStyle = BorderStyle.FixedSingle
+        hostPanel.BackColor = Color.Green
+
+        Dim AIdifficulty_lbl As New Label
+        fp.objectHandler.AddObject(hostPanel, AIdifficulty_lbl, 20, 20, 30, 120, "AI difficulty:")
+        AIdifficulty_lbl.Font = New Font("Arial", 15)
+        AIdifficulty_lbl.TextAlign = ContentAlignment.MiddleRight
+
+        Dim AIdifficulty_sel As New ComboBox
+        fp.objectHandler.AddObject(hostPanel, AIdifficulty_sel, 20, 160, 30, 120, "")
+        AIdifficulty_sel.Items.AddRange({"Easy", "Medium", "Hard"})
+
+        Dim startServer_but As New Button
+        fp.objectHandler.AddButton(hostPanel, startServer_but, 70, 20, 30, 120, "Start Server", AddressOf dnsModel.StartServer)
+
+        Dim showIPs_verbose_but As New Button
+        fp.objectHandler.AddButton(hostPanel, showIPs_verbose_but, 20, 300, 30, 120, "Show local IPs", AddressOf dnsModel.ListDNS_Addresses)
+    End Sub
+
+    Private Sub LogSetup(Log As TextBox, dispStr As String)
         Log.Multiline = True
         Log.ReadOnly = True
         Log.ScrollBars = ScrollBars.Vertical
         Log.BackColor = Color.Black
         Log.ForeColor = Color.White
         Log.Font = New Font("Courier New", 15)
+        WriteToLog(Log, dispStr)
     End Sub
 
     Private Sub ClientConnect(ipStr As String)
